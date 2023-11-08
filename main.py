b57b66ba8b0a53157a5089cf91e1a8ed655f6072
Nikrik <59827297+Nikrik@users.noreply.github.com>
2023-11-08 10:37:06 +0300

    my polling implementation

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import asyncio
 import os
 from datetime import datetime
 import time
+from telebot.types import Message
+from telebot import ExceptionHandler
+from telebot.handler_backends import HandlerBackend
+from telebot.storage import StateStorageBase
+import http.client
 
 
 async def try_delete_file(filename):
@@ -19,6 +24,71 @@ async def try_delete_file(filename):
         except PermissionError as e:
             if not (attempt < (max_attempts - 1)):
                 raise e
+
+
+class MyTelegramClient(telebot.TeleBot):
+    # Переопределяем класс, что бы починить его отрубание
+    def __init__(self, token: str, welcome_event_handler=None, message_event_handler=None, parse_mode: str | None = None, threaded: bool | None = True, skip_pending: bool | None = False, num_threads: int | None = 2, next_step_backend: HandlerBackend | None = None, reply_backend: HandlerBackend | None = None, exception_handler: ExceptionHandler | None = None, last_update_id: int | None = 0, suppress_middleware_excepions: bool | None = False, state_storage: StateStorageBase | None = ..., use_class_middlewares: bool | None = False, disable_web_page_preview: bool | None = None, disable_notification: bool | None = None, protect_content: bool | None = None, allow_sending_without_reply: bool | None = None, colorful_logs: bool | None = False):
+        self.__is_polling = False
+        # self.set_messa
+        super().__init__(token, parse_mode, threaded, skip_pending, num_threads, next_step_backend, reply_backend, exception_handler, last_update_id, suppress_middleware_excepions,
+                         state_storage, use_class_middlewares, disable_web_page_preview, disable_notification, protect_content, allow_sending_without_reply, colorful_logs)
+        self.message_event_handler = message_event_handler
+        self.welcome_event_handler = welcome_event_handler
+
+    def set_message_event_handler(self, function):
+        self.message_event_handler = function
+
+    def set_welcome_event_handler(self, function):
+        self.welcome_event_handler = function
+
+    def __poll(self):
+        connection = http.client.HTTPSConnection("api.telegram.org")
+        offset = None
+        # Пропустить сообщения, которые бот получит, когда был неактивным
+        if False:
+            url = f"/bot{self.token}/getUpdates"
+            connection.request("GET", url)
+            response = json.loads(connection.getresponse().read().decode("utf-8"))
+
+        while self.__is_polling:
+            url = f"/bot{self.token}/getUpdates?timeout=5{f'&offset={offset}' if offset else ''}"
+            connection.request("GET", url)
+            response = json.loads(connection.getresponse().read().decode("utf-8"))
+            if response['ok'] == True:
+                result = response['result']
+                if result:
+                    offset = result[-1]['update_id']+1
+                    for item in result:
+                        if 'message' in item.keys():
+                            message_object = Message.de_json(item['message'])
+                            if message_object:
+                                if self.message_event_handler != None:
+                                    if message_object.content_type == "new_chat_members":
+                                        self.welcome_event_handler(message_object)
+                                    else:
+                                        self.message_event_handler(message_object)
+                        else:
+                            text = f"Тип {item.keys()} не учтён"
+                            print(text)
+                            report_bug(text)
+                else:
+                    offset = None
+                time.sleep(1)
+            else:
+                report_bug(f"not ok {response}")
+
+    def start(self):
+        if self.__is_polling == False:
+            self.__is_polling = True
+            self.__poll()
+            print("im running")
+        else:
+            raise RuntimeError("MyTelegramClient is already running")
+
+    def stop(self):
+        self.__is_polling = False
+        print("im stop polling")
 
 
 class MyDiscordClient(discord.Client):
@@ -45,11 +115,8 @@ class MyDiscordClient(discord.Client):
             await ds_channel.send(message)
 
 
-def main():
-    print("Запуск главного потока")
-    with open('settings.json') as f:
-        data = json.load(f)
-
+with open('settings.json') as f:
+    data = json.load(f)
     ds_token = data['ds_token']
     tg_token = data['tg_token']
     ds_channel_id = data['ds_channel_id']
@@ -57,7 +124,13 @@ def main():
     ds_admin_chat_id = data['ds_admin_chat_id']
     ds_owner_chat_id = data['ds_owner_chat_id']
 
-    tg_content_types = ['text', 'audio', 'photo', 'voice', 'video', 'document', 'location', 'contact', 'sticker']
+
+def report_bug(message):
+    ds_bot.send_message(ds_admin_chat_id, message)
+
+
+def main():
+    print("Запуск главного потока")
     ds_content_type = "attachments"
 
     intents = discord.Intents(messages=True, message_content=True)
@@ -78,9 +151,8 @@ def main():
             async def on_ready():
                 global pid
                 pid = os.getpid()
-                readytext = f"Бот запущен под PID: {pid}\nДоступные команды: kill, restart, restart DS, restart TG"
+                readytext = f"Бот запущен под PID: {pid}"
                 ds_bot.send_message(ds_admin_chat_id, readytext)
-                # ds_bot.send_message(ds_owner_chat_id, readytext)
 
             # Пересылание сообщений из Discord в Telegram
 
@@ -113,28 +185,25 @@ def main():
                             errortext += f"\n{str(e)}\n"
                             errdate = message.created_at.astimezone().isoformat()
                             errortext += f"ID Сообщения: {message.id} от {errdate}"
-                            ds_bot.send_message(ds_admin_chat_id, errortext)
+                            report_bug(errortext)
                             raise e
                     # Обработка входящих команд владельца и админа
                     elif message.channel.id in [ds_admin_chat_id, ds_owner_chat_id]:
                         if message.content == "kill":
                             global is_killed
                             is_killed = True
-                            tg_bot.stop_bot()
+                            tg_bot.stop()
                             await ds_bot.close()
                         elif message.content == "restart":
                             global is_restarted
                             is_restarted = True
-                            tg_bot.stop_bot()
+                            tg_bot.stop()
                             await ds_bot.close()
                         elif message.content == "restart DS":
                             await ds_bot.close()
-                        elif message.content == "restart TG":
-                            text = "В данной версии бот сам управляет своими потоками, эта команда отключена"
-                            ds_bot.send_message(message.channel.id, text)
                         else:
-                            text = "Неизвестная команда\nДоступные команды: kill, restart, restart DS, restart TG"
-                            ds_bot.send_message(message.channel.id, text)
+                            text = "Неизвестная команда\nДоступные команды: kill, restart, restart DS"
+                            report_bug(text)
             ds_bot.run(ds_token)
             ds_bot.clear()
             print("Дискорд завершает свою работу")
@@ -147,14 +216,14 @@ def main():
         def restart_tg():
             print("Таймер запущен")
             time.sleep(60*60)
-            tg_bot.stop_bot()
+            tg_bot.stop()
             print("Бот остановлен по таймеру")
 
         while not is_killed and not is_restarted:
             print("Телеграм запускается")
-            tg_bot = telebot.TeleBot(tg_token)
+            tg_bot = MyTelegramClient(tg_token)
 
-            @tg_bot.message_handler(content_types=["new_chat_members"])
+            # @tg_bot.message_handler(content_types=["new_chat_members"])
             def welcome(message):
                 if message.chat.id == tg_chat_id:
                     try:
@@ -169,11 +238,11 @@ def main():
                         errortext += f"\n{str(e)}\n"
                         errdate = errdate = datetime.fromtimestamp(message.date).astimezone().isoformat()
                         errortext += f"ID Сообщения: {message.id} от {errdate}"
-                        ds_bot.send_message(ds_admin_chat_id, errortext, disable_web_page_preview=False)
+                        report_bug(errortext)
                         raise e
 
             # Баг, через некоторое время перестаёт получать сообщения
-            @tg_bot.message_handler(content_types=tg_content_types)
+            # @tg_bot.message_handler(content_types=tg_content_types)
             def get_message(message):
                 fileID = None
                 # ds_bot.send_message(ds_admin_chat_id, f"Сообщение из ID чата {message.chat.id},\n содержание\n{message.text}")
@@ -208,17 +277,19 @@ def main():
                             # asyncio.run(try_delete_file(file_info.file_path))
                         else:
                             errortext = f"Тип данных {message.content_type} не учтён"
-                            ds_bot.send_message(ds_admin_chat_id, errortext)
+                            report_bug(errortext)
                     except Exception as e:
                         errortext = f"Произошла ошибка при пересыланиии сообщения из телеграма в дискорд на строке {e.__traceback__.tb_lineno}:"
                         errortext += f"\n{str(e)}\n"
                         errdate = datetime.fromtimestamp(message.date).astimezone().isoformat()
                         errortext += f"ID Сообщения: {message.id} от {errdate}\n fileID={fileID}"
-                        ds_bot.send_message(ds_admin_chat_id, errortext)
+                        report_bug(errortext)
                         raise e
             # tg_restart_thread = Thread(target=restart_tg)
             # tg_restart_thread.start()
-            tg_bot.polling(non_stop=True, interval=1)
+            tg_bot.set_message_event_handler(get_message)
+            tg_bot.set_welcome_event_handler(welcome)
+            tg_bot.start()
             # tg_restart_thread.join()
             print("Телеграм завершает свою работу")
 
